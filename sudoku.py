c23719e0431ffdb6b2565d3056791dbfb914af56
ShadyZOZ <shadyzoz@163.com>
2015-06-17 02:25:07 +0800

    complete search and found functions
    compressed list box

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,6 @@
 board = []
 coordinate = []
-box = (
-        ((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)),
-        ((0, 3), (0, 4), (0, 5), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5)),
-        ((0, 6), (0, 7), (0, 8), (1, 6), (1, 7), (1, 8), (2, 6), (2, 7), (2, 8)),
-        ((3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (5, 0), (5, 1), (5, 2)),
-        ((3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)),
-        ((3, 6), (3, 7), (3, 8), (4, 6), (4, 7), (4, 8), (5, 6), (5, 7), (5, 8)),
-        ((6, 0), (6, 1), (6, 2), (7, 0), (7, 1), (7, 2), (8, 0), (8, 1), (8, 2)),
-        ((6, 3), (6, 4), (6, 5), (7, 3), (7, 4), (7, 5), (8, 3), (8, 4), (8, 5)),
-        ((6, 6), (6, 7), (6, 8), (7, 6), (7, 7), (7, 8), (8, 6), (8, 7), (8, 8))
-        )
+box = (((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)), ((0, 3), (0, 4), (0, 5), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5)), ((0, 6), (0, 7), (0, 8), (1, 6), (1, 7), (1, 8), (2, 6), (2, 7), (2, 8)), ((3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (5, 0), (5, 1), (5, 2)), ((3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)), ((3, 6), (3, 7), (3, 8), (4, 6), (4, 7), (4, 8), (5, 6), (5, 7), (5, 8)), ((6, 0), (6, 1), (6, 2), (7, 0), (7, 1), (7, 2), (8, 0), (8, 1), (8, 2)), ((6, 3), (6, 4), (6, 5), (7, 3), (7, 4), (7, 5), (8, 3), (8, 4), (8, 5)), ((6, 6), (6, 7), (6, 8), (7, 6), (7, 7), (7, 8), (8, 6), (8, 7), (8, 8)))
 
 def initBoard():
     for i in range(9):
@@ -25,12 +15,16 @@ def inputNumbers():
     if len(seq) == 81:
         n = 0
         for num in seq:
-            board[int(n / 9)][n % 9] = int(num)
-            coordinate[int(num)][locateBox(int(n / 9), n % 9)] = [int(n / 9), n % 9]
-            n += 1
+            x = int(n / 9)
+            y = n % 9
+            if not foundNumber(int(num), x, y) or num == '0':
+                board[x][y] = int(num)
+                coordinate[int(num)][locateBox(x, y)] = [x, y]
+                n += 1
+            else:
+                return 0
         return 1
     else:
-        print('please input the right number sequence')
         return 0
 
 def outputBoard():
@@ -54,16 +48,34 @@ def locateBox(x, y):
         if (x, y) in box[i]:
             return i
 
-def searchBox():
-    pass
+def searchBox(num, x, y):
+    try_ord = box[locateBox(x, y)]
+    for ords in try_ord:
+        if board[ords[0]][ords[1]] == num:
+            return 1
+    return 0
 
-def searchRow():
-    pass
+def searchRow(num, x):
+    for i in range(9):
+        if board[x][i] == num:
+            return 1
+    return 0
 
-def searchCol():
-    pass
+def searchCol(num, y):
+    for i in range(9):
+        if board[i][y] == num:
+            return 1
+    return 0
+
+def foundNumber(num, x, y):
+    if searchBox(num, x, y) or searchRow(num, x) or searchCol(num, y):
+        return 1
+    else:
+        return 0
 
 initBoard()
 initCoordinate()
 if inputNumbers():
     outputBoard()
+else:
+    print('please input the right number sequence')
