6df588ea593d44651f191ecf1e190f2e5ba17083
ShadyZOZ <shadyzoz@163.com>
2015-06-17 08:27:37 +0800

    half done

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,7 @@
 board = []
 coordinate = [[]]
 block = (((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)), ((0, 3), (0, 4), (0, 5), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5)), ((0, 6), (0, 7), (0, 8), (1, 6), (1, 7), (1, 8), (2, 6), (2, 7), (2, 8)), ((3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (5, 0), (5, 1), (5, 2)), ((3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)), ((3, 6), (3, 7), (3, 8), (4, 6), (4, 7), (4, 8), (5, 6), (5, 7), (5, 8)), ((6, 0), (6, 1), (6, 2), (7, 0), (7, 1), (7, 2), (8, 0), (8, 1), (8, 2)), ((6, 3), (6, 4), (6, 5), (7, 3), (7, 4), (7, 5), (8, 3), (8, 4), (8, 5)), ((6, 6), (6, 7), (6, 8), (7, 6), (7, 7), (7, 8), (8, 6), (8, 7), (8, 8)))
+stack = [0]
 
 def initBoard():
     for i in range(9):
@@ -9,14 +10,13 @@ def initBoard():
 def initCoordinate():
     for j in range(9):
         coordinate.append([[9, 9], [9, 9], [9, 9], [9, 9], [9, 9], [9, 9], [9, 9], [9, 9], [9, 9]])
-    coordinate.append([])
 
-def fill(num, x, y):
+def setNumber(num, x, y):
     board[x][y] = num
     if num == 0:
         coordinate[0].append([x, y])
     else:
-        coordinate[10].append([x, y])
+        coordinate[0].remove([x, y])
         coordinate[num][locateBlock(x, y)] = [x, y]
 
 def inputNumbers():
@@ -27,7 +27,6 @@ def inputNumbers():
             x = int(n / 9)
             y = n % 9
             if not foundNumber(int(num), x, y) or num == '0':
-                fill(int(num), x, y)
                 n += 1
             else:
                 return 0
@@ -83,15 +82,6 @@ def foundNumber(num, x, y):
 
 def easyfill_BlankBased():
     for [x, y] in coordinate[0]:
-        if [x, y] not in coordinate[10]:
-            n = 0
-            for i in range(1, 10):
-                if not foundNumber(i, x, y):
-                    n += 1
-                    temp = i
-            if n == 1:
-                fill(temp, x, y)
-                return 1
     return 0
 
 def easyfill_BlockBased():
@@ -100,33 +90,20 @@ def easyfill_BlockBased():
             if coordinate[i][j] == [9, 9]:
                 n = 0
                 for (x, y) in block[j]:
-                    if not foundNumber(i, x, y) and [x, y] not in coordinate[10]:
                         n += 1
                         temp = (x, y)
                 if n == 1:
-                    fill(i, temp[0], temp[1])
                     return 1
     return 0
 
-def main():
-    initBoard()
-    initCoordinate()
-    if inputNumbers():
-        outputBoard()
-    else:
-        print('please input the right number sequence')
     left = 0
-    newLeft = 81 - len(coordinate[10])
-    print(newLeft)
     while newLeft != left:
         left = newLeft
         while easyfill_BlankBased():
             pass
         while easyfill_BlockBased():
             pass
-        newLeft = 81 - len(coordinate[10])
-    outputBoard()
-    print(newLeft)
+
 
 if __name__ == '__main__':
     main()
